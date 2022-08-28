48976a372630f58f76c784d0032da69dcd675494
jf <2361400512@qq.com>
2022-08-28 16:52:47 +0800

    four

---
+++ today_one/src/main/java/Four.java
@@ -0,0 +1,10 @@
+/**
+ * @Description :
+ * @Author: JiFei
+ * @Date :2022/8/27 23:09
+ */
+public class Four {
+    public static void main(String[] args) {
+        System.out.println("four");
+    }
+}
