d9691509c9853933a1f864bda8220f6044857fa8
jf <2361400512@qq.com>
2022-08-28 17:01:44 +0800

    four

---
+++ today_one/src/main/java/Five.java
@@ -0,0 +1,10 @@
+/**
+ * @Description :
+ * @Author: JiFei
+ * @Date :2022/8/28 17:01
+ */
+public class Five {
+    public static void main(String[] args) {
+        System.out.println("five");
+    }
+}
