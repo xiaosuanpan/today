80ddf0deb83f6faf1c12884934fde4eef96681df
jf <2361400512@qq.com>
2022-08-28 17:05:09 +0800

    ten

---
+++ today_one/src/main/java/Ten.java
@@ -0,0 +1,12 @@
+
+
+/**
+ * @Description :
+ * @Author: JiFei
+ * @Date :2022/8/28 17:04
+ */
+public class Ten {
+    public static void main(String[] args) {
+        System.out.println("ten");
+    }
+}
